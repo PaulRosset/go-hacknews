372c773eb1e0a88a40e7530ae775cb820a4a1b3a
devsupa <paulrosset96@gmail.com>
2017-08-12 10:14:25 +0200

    Syntax test/

--- test/codes_test.go
+++ test/codes_test.go
@@ -11,9 +11,12 @@ import (
 func TestGetCodesTopStories(t *testing.T) {
 	init := hacknews.Initializer{"topstories", 40}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetCodesTopStories] Expected error to be nil, got : ", err)
-	} else if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
+	} 
+	
+	if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
 		t.Error("[TestGetCodesTopStories] Expected codes to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", codes, len(codes), cap(codes))
 	}
 }
@@ -21,9 +24,12 @@ func TestGetCodesTopStories(t *testing.T) {
 func TestGetCodeNewStories(t *testing.T) {
 	init := hacknews.Initializer{"newstories", 30}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetCodeNewStories] Expected error to be nil, got : ", err)
-	} else if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
+	} 
+	
+	if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
 		t.Error("[TestGetCodeNewStories] Expected codes to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", codes, len(codes), cap(codes))
 	}
 }
@@ -31,9 +37,12 @@ func TestGetCodeNewStories(t *testing.T) {
 func TestGetCodesBestStories(t *testing.T) {
 	init := hacknews.Initializer{"beststories", 20}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetCodesBestStories] Expected error to be nil, got : ", err)
-	} else if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
+	} 
+	
+	if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
 		t.Error("[TestGetCodesBestStories] Expected codes to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", codes, len(codes), cap(codes))
 	}
 }
@@ -41,9 +50,12 @@ func TestGetCodesBestStories(t *testing.T) {
 func TestGetCodesAskStories(t *testing.T) {
 	init := hacknews.Initializer{"askstories", 15}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetCodesAskStories] Expected error to be nil, got : ", err)
-	} else if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
+	} 
+	
+	if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
 		t.Error("[TestGetCodesAskStories] Expected codes to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", codes, len(codes), cap(codes))
 	}
 }
@@ -51,9 +63,12 @@ func TestGetCodesAskStories(t *testing.T) {
 func TestGetCodesShowStories(t *testing.T) {
 	init := hacknews.Initializer{"showstories", 10}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetCodesShowStories] Expected error to be nil, got : ", err)
-	} else if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
+	} 
+	
+	if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
 		t.Error("[TestGetCodesShowStories] Expected codes to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", codes, len(codes), cap(codes))
 	}
 }
@@ -61,9 +76,12 @@ func TestGetCodesShowStories(t *testing.T) {
 func TestGetCodesJobStories(t *testing.T) {
 	init := hacknews.Initializer{"jobstories", 5}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetCodesJobStories] Expected error to be nil, got : ", err)
-	} else if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
+	} 
+	
+	if codes == nil || len(codes) == 0 || len(codes) <= init.NbPosts || cap(codes) == 0 {
 		t.Error("[TestGetCodesJobStories] Expected codes to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", codes, len(codes), cap(codes))
 	}
 }
--- test/posts_test.go
+++ test/posts_test.go
@@ -11,13 +11,18 @@ import (
 func TestGetPostTopStories(t *testing.T) {
 	init := hacknews.Initializer{"topstories", 40}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetPostTopStories] Expected error to be nil, got : ", err)
 	}
+	
 	posts, errPost := init.GetPostStory(codes)
+	
 	if errPost != nil {
 		t.Error("[TestGetPostTopStories] Expected error to be nil, got : ", err)
-	} else if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
+	} 
+	
+	if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
 		t.Error("[TestGetPostTopStories] Expected posts to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", posts, len(posts), cap(posts))
 	}
 	for _, post := range posts {
@@ -30,15 +35,21 @@ func TestGetPostTopStories(t *testing.T) {
 func TestGetPostNewStories(t *testing.T) {
 	init := hacknews.Initializer{"newstories", 30}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetPostNewStories] Expected error to be nil, got : ", err)
 	}
+	
 	posts, errPost := init.GetPostStory(codes)
+	
 	if errPost != nil {
 		t.Error("[TestGetPostNewStories] Expected error to be nil, got : ", err)
-	} else if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
+	} 
+	
+	if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
 		t.Error("[TestGetPostNewStories] Expected posts to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", posts, len(posts), cap(posts))
 	}
+
 	for _, post := range posts {
 		if post.Id == 0 {
 			t.Error("[TestGetPostNewStories] Id is mandatory, got : ", post.Id)
@@ -49,15 +60,21 @@ func TestGetPostNewStories(t *testing.T) {
 func TestGetPostBestStories(t *testing.T) {
 	init := hacknews.Initializer{"beststories", 20}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetPostBestStories] Expected error to be nil, got : ", err)
 	}
+	
 	posts, errPost := init.GetPostStory(codes)
+	
 	if errPost != nil {
 		t.Error("[TestGetPostBestStories] Expected error to be nil, got : ", err)
-	} else if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
+	} 
+	
+	if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
 		t.Error("[TestGetPostBestStories] Expected posts to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", posts, len(posts), cap(posts))
 	}
+	
 	for _, post := range posts {
 		if post.Id == 0 {
 			t.Error("[TestGetPostBestStories] Id is mandatory, got : ", post.Id)
@@ -68,15 +85,21 @@ func TestGetPostBestStories(t *testing.T) {
 func TestGetPostAskStories(t *testing.T) {
 	init := hacknews.Initializer{"askstories", 15}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetPostAskStories] Expected error to be nil, got : ", err)
 	}
+
 	posts, errPost := init.GetPostStory(codes)
+	
 	if errPost != nil {
 		t.Error("[TestGetPostAskStories] Expected error to be nil, got : ", err)
-	} else if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
+	} 
+	
+	if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
 		t.Error("[TestGetPostAskStories] Expected posts to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", posts, len(posts), cap(posts))
 	}
+
 	for _, post := range posts {
 		if post.Id == 0 {
 			t.Error("TestGetPostAskStories] Id is mandatory, got : ", post.Id)
@@ -87,15 +110,21 @@ func TestGetPostAskStories(t *testing.T) {
 func TestGetPostShowStories(t *testing.T) {
 	init := hacknews.Initializer{"showstories", 10}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetPostShowStories] Expected error to be nil, got : ", err)
 	}
+
 	posts, errPost := init.GetPostStory(codes)
+	
 	if errPost != nil {
 		t.Error("[TestGetPostShowStories] Expected error to be nil, got : ", err)
-	} else if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
+	} 
+	
+	if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
 		t.Error("[TestGetPostShowStories] Expected posts to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", posts, len(posts), cap(posts))
 	}
+
 	for _, post := range posts {
 		if post.Id == 0 {
 			t.Error("TestGetPostShowStories] Id is mandatory, got : ", post.Id)
@@ -106,15 +135,21 @@ func TestGetPostShowStories(t *testing.T) {
 func TestGetPostJobStories(t *testing.T) {
 	init := hacknews.Initializer{"jobstories", 5}
 	codes, err := init.GetCodesStory()
+	
 	if err != nil {
 		t.Error("[TestGetPostJobStories] Expected error to be nil, got : ", err)
 	}
+
 	posts, errPost := init.GetPostStory(codes)
+	
 	if errPost != nil {
 		t.Error("[TestGetPostJobStories] Expected error to be nil, got : ", err)
-	} else if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
+	} 
+	
+	if posts == nil || len(posts) == 0 || len(posts) != init.NbPosts || cap(posts) == 0 {
 		t.Error("[TestGetPostJobStories] Expected posts to be not nil or have a length > 0 and atleast > to the number of posts asked or capacity > 0, got : ", posts, len(posts), cap(posts))
 	}
+	
 	for _, post := range posts {
 		if post.Id == 0 {
 			t.Error("TestGetPostJobStories] Id is mandatory, got : ", post.Id)
